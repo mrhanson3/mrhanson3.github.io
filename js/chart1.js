16d18e1b024f0ffc2f147a9c821295acf4b5d434
Clayton <cahanson12@gmail.com>
2018-08-18 12:19:36 -0400

    Add files via upload

--- js/chart1.js
+++ js/chart1.js
@@ -3,19 +3,19 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-    labels: ["Aug-10", "Aug-16"],
+    labels: ["Aug-10", "Aug-16", "Aug-18"],
     datasets: [{ 
-        data: [10, 9.52],
+        data: [10, 9.52, 9.47],
         label: "Bitcoin",
         borderColor: "#3e95cd",
         fill: false
       }, { 
-        data: [10, 7.86],
+        data: [10, 7.86, 7.80],
         label: "Ethereum",
         borderColor: "#8e5ea2",
         fill: false
       }, { 
-        data: [10, 8.80],
+        data: [10, 8.80, 8,91],
         label: "Litecoin",
         borderColor: "#3cba9f",
         fill: false
@@ -26,10 +26,12 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero:true,
+                    max: 30,
+                    min: 0,
+                    stepSize: 5                 
                 }
             }]
-
         }
-    }
+      }  
 });
