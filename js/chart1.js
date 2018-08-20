a84f0fdce52cc60bcc5fbd70f715fc717db678fd
Clayton <cahanson12@gmail.com>
2018-08-20 10:14:08 -0400

    Updated crypto chart.

--- js/chart1.js
+++ js/chart1.js
@@ -3,19 +3,19 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-    labels: ["Aug-10", "Aug-16", "Aug-18"],
+    labels: ["Aug-10", "Aug-16", "Aug-18", "Aug-20"],
     datasets: [{ 
-        data: [10, 9.52, 9.47],
+        data: [10.25, 9.52, 9.47, 9.53],
         label: "Bitcoin",
         borderColor: "#3e95cd",
         fill: false
       }, { 
-        data: [10, 7.86, 7.80],
+        data: [10.25, 7.86, 7.80, 7.69],
         label: "Ethereum",
         borderColor: "#8e5ea2",
         fill: false
       }, { 
-        data: [10, 8.80, 8,91],
+        data: [10.25, 8.80, 8.91, 8.59],
         label: "Litecoin",
         borderColor: "#3cba9f",
         fill: false
