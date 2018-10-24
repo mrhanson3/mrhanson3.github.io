dee4dda4d206de8f5cd5cf75c36987204fd861db
Clayton <cahanson12@gmail.com>
2018-10-24 05:21:21 -0400

    Updated data.

--- js/chart1.js
+++ js/chart1.js
@@ -3,34 +3,34 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-    labels: ["Aug-10", "Aug-24", "Sept-7", "Sept-21", "Oct-5"],
+    labels: ["Aug-10", "Aug-24", "Sept-7", "Sept-21", "Oct-5", "Oct-19"],
     datasets: [{ 
-        data: [10.25, 10.22, 16.80, 22.17, 29.00],
+        data: [10.25, 10.22, 16.80, 22.17, 29.00, 38.79],
         label: "Bitcoin",
         borderColor: "#0000ff",
         fill: false
       }, { 
-        data: [10.25, 7.40, 11.64, 17.08, 23.12],
+        data: [10.25, 7.40, 11.64, 17.08, 23.12, 30.60],
         label: "Ethereum",
         borderColor: "#8e5ea2",
         fill: false
       }, { 
-        data: [10.25, 8.81, 15.73, 21.23, 27.93],
+        data: [10.25, 8.81, 15.73, 21.23, 27.93, 35.06],
         label: "Litecoin",
         borderColor: "#00ff00",
         fill: false
       }, { 
-        data: [67.61, 67.61, 91.61, 109.61, 133.61],
+        data: [67.61, 67.61, 91.61, 109.61, 133.61, 169.61],
         label: "Total Cost",
         borderColor: "#000000",
         fill: false
       }, { 
-        data: [67.61, 60.50, 86.98, 104.33, 122.76],
+        data: [67.61, 60.50, 86.98, 104.33, 122.76, 145.28],
         label: "Total Value",
         borderColor: "#ff0000",
         fill: false
       }, { 
-        data: [36.86, 34.07, 42.81, 43.85, 42.71],
+        data: [36.86, 34.07, 42.81, 43.85, 42.71, 40.83],
         label: "Monero",
         borderColor: "#ffa500",
         fill: false
