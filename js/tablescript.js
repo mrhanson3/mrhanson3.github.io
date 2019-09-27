6c3ddeaffea416c7aa7c1637ac27fa93e20977a3
Clayton <cahanson12@gmail.com>
2019-09-27 04:52:58 -0400

    Update tablescript.js

--- js/tablescript.js
+++ js/tablescript.js
@@ -57,8 +57,8 @@ const people = [
 { date: '9/23/19', bet: '$30', towin: '$48',  result: '', description: 'Patriots to win the AFC'},
 { date: '9/23/19', bet: '$30', towin: '$82.50',  result: '', description: 'Chiefs to win the AFC'},
 { date: '9/23/19', bet: '$20', towin: '$66.60',  result: '', description: 'Patriots to win the Super Bowl'},
-{ date: '9/23/19', bet: '$10', towin: '$9.52',  result: '-$10', description: 'Eagles @ Packers over 47'},
-{ date: '9/23/19', bet: '$20', towin: '$15.38',  result: '-$20', description: '@ Packers -3.5 over Eagles'}];
+{ date: '9/26/19', bet: '$10', towin: '$9.52',  result: '-$10', description: 'Eagles @ Packers over 47'},
+{ date: '9/26/19', bet: '$20', towin: '$15.38',  result: '-$20', description: '@ Packers -3.5 over Eagles'}];
 
 
 
