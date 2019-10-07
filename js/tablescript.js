b82826fd1a5b37c6eb169723b669eaeaf187eb93
Clayton <cahanson12@gmail.com>
2019-10-07 13:25:02 -0400

    Update tablescript.js

--- js/tablescript.js
+++ js/tablescript.js
@@ -61,11 +61,13 @@ const people = [
 { date: '9/26/19', bet: '$20', towin: '$15.38',  result: '-$20', description: '@ Packers -3.5 over Eagles'},
 { date: '9/29/19', bet: '$10', towin: '$8',  result: '$8', description: 'Patriots @ Bills under 40.5'},
 { date: '9/29/19', bet: '$10', towin: '$8.33',  result: '$8.33', description: 'Vikings @ Bears under 38.5'},
-{ date: '9/29/19', bet: '$13.92', towin: '$16.01',  result: '', description: 'Rams over @ Seahawks'},
-{ date: '9/29/19', bet: '$7', towin: '$6.36',  result: '', description: 'Bears @ Raiders Under 41'},
-{ date: '9/29/19', bet: '$13.96', towin: '$5.91',  result: '', description: 'Bears over @ Raiders'},
-{ date: '9/29/19', bet: '$10', towin: '$2.22',  result: '', description: 'Two Team Parlay: Patriots over Washington and @ Eagles over Jets'},
-{ date: '9/29/19', bet: '$100', towin: '$9.09',  result: '', description: 'Patriots over @ Washington'}];
+{ date: '9/29/19', bet: '$13.92', towin: '$16.01',  result: '-$13.92', description: 'Rams over @ Seahawks'},
+{ date: '9/29/19', bet: '$7', towin: '$6.36',  result: '-$7', description: 'Bears @ Raiders Under 41'},
+{ date: '9/29/19', bet: '$13.96', towin: '$5.91',  result: "-$13.96'', description: 'Bears over @ Raiders'},
+{ date: '9/29/19', bet: '$10', towin: '$2.22',  result: '$2.22', description: 'Two Team Parlay: Patriots over Washington and @ Eagles over Jets'},
+{ date: '9/29/19', bet: '$100', towin: '$9.09',  result: '$9.09', description: 'Patriots over @ Washington'},
+{ date: '9/29/19', bet: '$10', towin: '$6.25',  result: '$6.25', description: '@ Panthers over @ Jaguars'},
+{ date: '9/29/19', bet: '$20', towin: '$16.67',  result: '$-20', description: '@ Chiefs -10.5 over Colts'}];
 
 
 /*========================================================
