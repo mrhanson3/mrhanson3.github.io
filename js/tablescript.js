00db69bb9673637dceae85d69e29e28cbf296261
Clayton <cahanson12@gmail.com>
2020-01-06 08:19:11 -0500

    Update tablescript.js

--- js/tablescript.js
+++ js/tablescript.js
@@ -16,12 +16,7 @@ const clearInput = document.getElementById('clearInput');
 
 
 const people = [
-{ date: '8/16/19', bet: '$10', towin: '$10.50',  result: ' ', description: 'Colts under 6.5 wins'},
-{ date: '9/23/19', bet: '$30', towin: '$48',  result: '', description: 'Patriots to win the AFC'},
 { date: '9/23/19', bet: '$30', towin: '$82.50',  result: '', description: 'Chiefs to win the AFC'},
-{ date: '9/23/19', bet: '$20', towin: '$66.60',  result: '', description: 'Patriots to win the Super Bowl'},
-{ date: '10/29/19', bet: '$12', towin: '$30',  result: '', description: 'Saints to win the NFC'},
-{ date: '11/7/19', bet: '$20', towin: '$130',  result: '', description: 'Saints to win the Super Bowl'},
 { date: '11/7/19', bet: '$20', towin: '$140',  result: '', description: 'Ravens to win the Super Bowl'},
 { date: '12/11/19', bet: '$30', towin: '$66',  result: '', description: 'Ravens to win the Super Bowl'},
 { date: '12/11/19', bet: '$20', towin: '$13.79',  result: '', description: 'AFC to win the Super Bowl'}]
